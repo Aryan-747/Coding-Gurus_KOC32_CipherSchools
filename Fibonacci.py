c2602792de7f2dfe48cef094c289f3e7a12d67c8
Aryan <arayn.32@gmail.com>
2022-11-24 21:31:09 +0530

    Completed Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,30 @@
+num = list(map(int,input("Enter The Number(s): ").split()))
+
+for i in range(len(num)):
+    
+    if(num[i]< 0):
+        print(f"{num[i]} is an Invalid Fibonacci Number\n")
+    
+    else:
+
+        if(num[i] == 0 or num[i] == 1):
+            print(f"{num[i]} is a Valid Fibonacci Number\n")
+
+        else:
+            n1 = 0
+            n2 = 1
+            Sum = n1 + n2
+
+            while(Sum<num[i]):
+                Sum = n1 + n2
+                n1 = n2
+                n2 = Sum
+            
+            if(Sum == num[i]):
+                print(f"{num[i]} is a Valid Fibonacci Number\n")
+            else:
+                print(f"{num[i]} is an Invalid Fibonacci Number\n")
+
+
+            
+        
